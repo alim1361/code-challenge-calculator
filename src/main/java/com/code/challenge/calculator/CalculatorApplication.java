8aff11f91a3de0ed9c864f3c72ff5ce0ea249348
bahareh <bh.hoseini@gmail.com>
2020-01-15 00:15:39 +0100

    adding logging to application runner

--- src/main/java/com/code/challenge/calculator/CalculatorApplication.java
+++ src/main/java/com/code/challenge/calculator/CalculatorApplication.java
@@ -1,13 +1,18 @@
 package com.code.challenge.calculator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CalculatorApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
+        LOGGER.info("System has started");
     }
 
 }
